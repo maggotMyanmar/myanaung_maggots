f08ee3b1c18412f8abaa141372f609946c0e5491
maggotMyanmar <80409750+maggotMyanmar@users.noreply.github.com>
2021-03-10 21:57:01 +0630

    Delete MygLout directory

--- MygLout/abc.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelectorAll('img')
-var imagesArr = Array.prototype.slice.call(images);
-setTimeout(() => { 
-    imagesArr.map(image =>{
-        const height = image.clientHeight;
-        const spans = Math.ceil(height / 10)
-        image.style.gridRowEnd = `span ${spans} `
-   })
-}, 300);
